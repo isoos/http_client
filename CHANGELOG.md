940ab3f5b9ab81faa48ef8c7005e3e128d5e16e8
Istvan Soos <istvan.soos@gmail.com>
2017-03-08 01:15:41 +0100

    Exporting streaming request and response.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 0.1.0
 
 - Exporting classes from the `http` package (version: `^0.1.13`).
+
+### 0.1.0+1
+
+- Exporting streaming request and response.
--- lib/http_client.dart
+++ lib/http_client.dart
@@ -1,4 +1,5 @@
+export 'package:http/src/base_client.dart';
 export 'package:http/src/request.dart';
 export 'package:http/src/response.dart';
-export 'package:http/src/base_client.dart';
-
+export 'package:http/src/streamed_request.dart';
+export 'package:http/src/streamed_response.dart';
