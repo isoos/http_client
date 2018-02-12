39a194a854f91aecc19f9a1454f808ce0ed88d18
Istvan Soos <istvan.soos@gmail.com>
2018-02-12 21:42:38 +0100

    Added example

---
+++ example/example.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:http_client/console.dart';
+
+Future main() async {
+  final client = new ConsoleClient();
+  final rs = await client.send(new Request('GET', 'https://www.example.com/'));
+  final textContent = await rs.readAsString();
+  print(textContent);
+  await client.close();
+}
